a6062c36d076a93318a257d98e4109ee72459958
vitoralveschaves <vitorchavesalv@gmail.com>
2024-10-28 10:24:33 -0300

    feat: display listed data from api

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+import FilterDropdown from '@/components/filter-dropdown';
+import OrdersTable from '@/components/orders-table';
+import Pagination from '@/components/pagination';
+import SearchInput from '@/components/search-input';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import axios from 'axios';
+
+export default async function Component() {
+
+  const response = await axios.get("https://apis.codante.io/api/orders-api/orders");
+  const orders = response.data.data;
+
+  return (
+    <main className="container px-1 py-10 md:p-10">
+      <Card>
+        <CardHeader className="px-7">
+          <CardTitle>Pedidos</CardTitle>
+          <CardDescription>
+            Uma listagem de pedidos do seu negócio.
+          </CardDescription>
+          <div className="flex pt-10 gap-4">
+            <SearchInput />
+            <FilterDropdown />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <OrdersTable orders={orders} />
+          <div className="mt-8">
+            <Pagination />
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
---
+++ components/orders-table.tsx
@@ -0,0 +1,63 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Badge } from './ui/badge';
+import { ChevronsUpDown } from 'lucide-react';
+import { Orders } from '@/lib/types/types';
+
+type OrdersTableProps = {
+  orders: Orders[]
+}
+
+const formatter = new Intl.NumberFormat('pt-br', {
+  style: 'currency',
+  currency: 'BRL'
+})
+
+export default function OrdersTable({ orders }: OrdersTableProps) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow className="w-full">
+          <TableHead className="table-cell">Cliente</TableHead>
+          <TableHead className="table-cell">Status</TableHead>
+          <TableHead className="hidden md:table-cell cursor-pointer justify-end items-center gap-1">
+            <div className="flex items-center gap-1">
+              Data
+              <ChevronsUpDown className="w-4" />
+            </div>
+          </TableHead>
+          <TableHead className="text-right cursor-pointer flex justify-end items-center gap-1">
+            Valor
+            <ChevronsUpDown className="w-4" />
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+
+        {orders.map((order) => (
+          <TableRow key={order.id}>
+            <TableCell>
+              <div className="font-medium">{order.customer_name}</div>
+              <div className="hidden md:inline text-sm text-muted-foreground">
+                {order.customer_email}
+              </div>
+            </TableCell>
+            <TableCell>
+              <Badge className={`text-xs`} variant="outline">
+                {order.status === 'pending' ? 'Pendente' : 'Completo'}
+              </Badge>
+            </TableCell>
+            <TableCell className="hidden md:table-cell">{order.order_date.toString()}</TableCell>
+            <TableCell className="text-right">{formatter.format(order.amount_in_cents / 100)}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
